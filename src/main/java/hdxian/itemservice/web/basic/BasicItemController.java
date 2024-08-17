c739b5b60795e12d8c7a446828be7fbdd45a10b2
hdXian <hyun353500@naver.com>
2024-08-17 18:36:32 +0900

    #2 2-7 - add usage of redirectAttributes

--- src/main/java/hdxian/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hdxian/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -88,11 +89,23 @@ public class BasicItemController {
     }
 
     // apply PRG method
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV5(@ModelAttribute("item") Item item) {
-        itemRepository.save(item);
+        Item savedItem = itemRepository.save(item);
         // redirect to item info page
-        return "redirect:/basic/items/" + item.getId();
+        return "redirect:/basic/items/" + savedItem.getId();
+    }
+
+    // use RedirectAttributes
+    @PostMapping("/add")
+    public String addItemV6(@ModelAttribute("item") Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        // use redirectAttribute as a parameter (also encoded in auto)
+        // the other params generated as query parameter
+        return "redirect:/basic/items/{itemId}";
+        // ex. itemId = 1 -> redirect url is: /basic/items/1?status=true
     }
 
     @GetMapping("/{itemId}/edit")
